6850e8e618c7048a1841b07e85bcd9487502f3d7
Tony Archuleta <antonio.archuleta@gmail.com>
2017-08-02 13:09:08 -0600

    Adjusted Main class to use ContactList and MobilePHone objects

--- src/Main.java
+++ src/Main.java
@@ -4,47 +4,22 @@ import java.util.HashMap;
  * Created by ac003588 on 7/26/2017.
  */
 public class Main {
-
-    static HashMap test = new HashMap();
-
-
     public static void main(String[] args) {
-
-   MobilePhone phone = new MobilePhone();
-   phone.contacts.listContacts();
-   phone.ContactsMenu();
-
-
-//        int[] myIntArray = new int[10];
-//
-//        for (int i = 0; i < 10; i++) {
-//
-//            //for a variable i that is less than 10 and incremented by 1 each iteration
-//            // set myIntArray's index at 'i'(0-9) to i(0-9) * 10
-//            myIntArray[i] = i * 10;
-//        }
-//
-////        for (int i = 0; i < myIntArray.length; i++) {
-////            // for a variable i that is less than the array length
-////            // and incremented by i each iteration
-////            // print the element number and it's value, set above
-////            System.out.println("Element " + i + ", value is " + myIntArray[i]);
-////        }
-//        printArray(myIntArray);
-//    }
-//
-//    public static void printArray(int[] array) {
-//        for (int i = 0; i < array.length; i++) {
-//            // for a variable i that is less than the array length
-//            // and incremented by i each iteration
-//            // print the element number and it's value, set above
-//            System.out.println("Element " + i + ", value is " + array[i]);
-//
-//
+        // Create contact list
+        ContactList cl = new ContactList();
+//        Contact p1 = new Contact("bob", "smith");
+ //       cl.addContact(p1);
+//        Contact joe = cl.searchContacts("smith");
+//        if(joe == null) {
+//            System.out.println("Joe not found");
+//        } else {
+//            System.out.println(joe.toString());
 //        }
 
-//        ArrayTest a1 = new ArrayTest();
-//        a1.sortArray(5);
+
+MobilePhone m1 = new MobilePhone(cl);
+m1.ContactsMenu();
+
 
     }
 }
